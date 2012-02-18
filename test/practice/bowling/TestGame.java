ee5c5d4d0f82da72b7432cd4c01aafd55d2555d6
zhaohuabing <zhaohuabing@gmail.com>
2012-02-18 15:03:52 +0800

    完成练习代码

--- test/practice/bowling/TestGame.java
+++ test/practice/bowling/TestGame.java
@@ -4,45 +4,197 @@ import junit.framework.TestCase;
 
 public class TestGame extends TestCase
 {
+    private Game g;
 
-//    public void testoneThrow()
-//    {
-//	Game g = new Game();
-//	g.addThrow(5);
-//	assertEquals(5,g.score());
-//    }
-    
+    protected void setUp() throws Exception
+    {
+	g = new Game();
+    }
     public void testtwoThrow()
     {
-	Game g = new Game();
 	g.addThrow(5);
 	g.addThrow(4);
-	assertEquals(9,g.scoreForFrame(1));
+	assertEquals(9, g.scoreForFrame(1));
     }
-    
+
     public void testthreeThrow()
     {
-	Game g = new Game();
 	g.addThrow(5);
 	g.addThrow(5);
 	g.addThrow(4);
-	assertEquals(14,g.scoreForFrame(1));
-	assertEquals(14,g.score());
+	assertEquals(14, g.scoreForFrame(1));
+	assertEquals(14, g.score());
     }
-    
+
     public void testsixThrow()
     {
-	Game g = new Game();
 	g.addThrow(5);
 	g.addThrow(5);
+	
 	g.addThrow(10);
+	
 	g.addThrow(5);
 	g.addThrow(5);
+	
 	g.addThrow(4);
-	assertEquals(20,g.scoreForFrame(1));
-	assertEquals(40,g.scoreForFrame(2));
-	assertEquals(54,g.scoreForFrame(3));
-	assertEquals(54,g.score());
+	assertEquals(20, g.scoreForFrame(1));
+	assertEquals(40, g.scoreForFrame(2));
+	assertEquals(54, g.scoreForFrame(3));
+	assertEquals(54, g.score());
+    }
+    
+    public void testtenThrow()
+    {
+	g.addThrow(5);
+	g.addThrow(5);//20
+	
+	g.addThrow(10);//20
+	
+	g.addThrow(5);
+	g.addThrow(5);//14
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	assertEquals(20, g.scoreForFrame(1));
+	assertEquals(40, g.scoreForFrame(2));
+	assertEquals(54, g.scoreForFrame(3));
+	assertEquals(110, g.scoreForFrame(10));
+	assertEquals(110, g.score());
+    }
+    
+    public void testtenThrowSpare()
+    {
+	g.addThrow(5);
+	g.addThrow(5);//20
+	
+	g.addThrow(10);//20
+	
+	g.addThrow(5);
+	g.addThrow(5);//14
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	
+	g.addThrow(4);
+	g.addThrow(6);//16
+	
+	g.addThrow(6);//
+	assertEquals(20, g.scoreForFrame(1));
+	assertEquals(40, g.scoreForFrame(2));
+	assertEquals(54, g.scoreForFrame(3));
+	assertEquals(118, g.scoreForFrame(10));
+	assertEquals(118, g.score());
+    }
+
+    public void testtenThrowStrike()
+    {
+	g.addThrow(5);
+	g.addThrow(5);//20
+	
+	g.addThrow(10);//20
+	
+	g.addThrow(5);
+	g.addThrow(5);//14
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	
+	g.addThrow(10);//22
+	
+	g.addThrow(6);	
+	g.addThrow(6);
+	assertEquals(20, g.scoreForFrame(1));
+	assertEquals(40, g.scoreForFrame(2));
+	assertEquals(54, g.scoreForFrame(3));
+	assertEquals(124, g.scoreForFrame(10));
+	assertEquals(124, g.score());
+    }
+    
+    public void testtenThrowStrike10()
+    {
+	g.addThrow(5);
+	g.addThrow(5);//20
+	
+	g.addThrow(10);//20
+	
+	g.addThrow(5);
+	g.addThrow(5);//14
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	
+	g.addThrow(4);
+	g.addThrow(4);//8
+	
+	g.addThrow(10);//22
+	
+	g.addThrow(10);	
+	g.addThrow(10);
+	assertEquals(20, g.scoreForFrame(1));
+	assertEquals(40, g.scoreForFrame(2));
+	assertEquals(54, g.scoreForFrame(3));
+	assertEquals(132, g.scoreForFrame(10));
+	assertEquals(132, g.score());
     }
 
 }
